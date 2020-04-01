90ef41d4a8a6fd18e25c8d4b0511df0653f04d03
Jack Linhart <iatenine@gmail.com>
2020-03-31 23:25:01 -0600

    Foreground service working

--- app/src/main/java/com/example/locationtestproject/MainActivity.java
+++ app/src/main/java/com/example/locationtestproject/MainActivity.java
@@ -1,28 +1,21 @@
 package com.example.locationtestproject;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.nearby.messages.Message;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     final int FREQUENT_UDPATES = 1000;
     final int BATTERY_SAVER = 5000;
+    Intent i;
     Boolean boolCautious = true;
 
     Activity masterClass = this;
@@ -31,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i = new Intent(getApplicationContext(), serviceExtendedClass.class);
     }
 
     /*
@@ -38,16 +32,11 @@ public class MainActivity extends AppCompatActivity {
     *
     * */
     public void startBroadcast(View view){
-        Intent i = new Intent(getApplicationContext(), serviceExtendedClass.class);
+        ContextCompat.startForegroundService(getApplicationContext(), i);
+    }
 
-        //TODO
-        //GET THESE TO WORK!!!!
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            startForegroundService(i);
-        }
-        else{
-            startService(i);
-        }
+    public void stopBroadcast(View view){
+        stopService(i);
     }
 
     public void updateAgreement(View view){
@@ -59,50 +48,5 @@ public class MainActivity extends AppCompatActivity {
     public void flipCautious(View view){
         Switch cautiousSwtich = findViewById(R.id.switchConserve);
         boolCautious = cautiousSwtich.isChecked();
-        aToast("Cautious = " + boolCautious);
     }
-
-    public void aToast(String msg){
-        Toast t = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG);
-        t.show();
-    }
-
-    /*
-    public void requestTracking(){
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                setContentView(R.layout.activity_main);
-                locationManager =(LocationManager) getSystemService(LOCATION_SERVICE);
-
-                if (ActivityCompat.checkSelfPermission(masterClass, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                    if (ActivityCompat.checkSelfPermission(masterClass, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(masterClass, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 123);
-                        startTracking();
-                        return;
-                    }
-                    else{
-
-                    }
-                startTracking();
-            }
-        };
-
-        r.run();
-    }
-
-    public void startTracking(){
-        updateLastLoc();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, (float) 0.5, locationListener);
-    }
-
-    public void updateLastLoc(){
-        lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    }
-
-    private void publish(String message) {
-        mActiveMessage = new Message(message.getBytes());
-        Nearby.getMessagesClient(this).publish(mActiveMessage);
-    }
-     */
 }
--- app/src/main/java/com/example/locationtestproject/serviceExtendedClass.java
+++ app/src/main/java/com/example/locationtestproject/serviceExtendedClass.java
@@ -1,12 +1,15 @@
 package com.example.locationtestproject;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,11 +17,14 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.nearby.Nearby;
@@ -27,6 +33,9 @@ import com.google.android.gms.nearby.messages.MessageListener;
 
 public class serviceExtendedClass extends Service {
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
+    public static final int NOT_ID = 6450;
+    private static final String TAG = "Service Logger";
+
     private MessageListener messageListener;
     private Message message;
 
@@ -34,115 +43,149 @@ public class serviceExtendedClass extends Service {
     private LocationListener locationListener;
     private Location lastLoc;
 
+    private Notification notification;
+    private NotificationChannel notificationChannel;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-
-    public void aToast(String msg){
-        Toast t = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG);
-        t.show();
-    }
-
-    public void playTone(){
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
-        mp.start();
-    }
-
-
-    @SuppressLint("MissingPermission")
-    public void updateLastLoc(){
-        lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i("Info", "Got to service!!");
 
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-                notificationIntent, 0);
-
-        Notification notification = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Social Distancing Alerts")
-                .setContentText("A notification sound will play if any devices are detected at an unsafe distance")
-                .setContentIntent(pendingIntent).build();
-
-        startForeground(1337, notification);
-
-
-        Toast.makeText(this, "Hi there", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "serviceExtendedClass: onCreate() called");
+        createNotificationChannel();
+        startForeground(NOT_ID, notification);
 
         messageListener = new MessageListener() {
             @Override
             public void onFound(Message message) {
-                String strMessage = message.toString();
-                String[] coords = strMessage.split("\\s+");
-                Location otherLoc = new Location(LocationManager.GPS_PROVIDER);
-                updateLastLoc();
-
-                double lat = Double.parseDouble(coords[0]);
-                double lon = Double.parseDouble(coords[1]);
-
-                otherLoc.setLatitude(Double.parseDouble(coords[0]));
-                otherLoc.setLongitude(Double.parseDouble(coords[1]));
-
-                double gap = lastLoc.distanceTo(otherLoc);
-                aToast("Distance of " + gap);
+                Log.d(TAG, "onFound: Found message: " + message.toString());
             }
 
             @Override
             public void onLost(Message message) {
             }
         };
-        message = new Message("Hello world".getBytes());
+
+
+        Nearby.getMessagesClient(getBaseContext()).subscribe(messageListener);
+        requestTracking();
+        updateLastLoc();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        Log.d("Service", "exist aici");
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
-        Nearby.getMessagesClient(this).publish(message);
-        Nearby.getMessagesClient(this).subscribe(messageListener);
-
-        createNotificationChannel();
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                0, notificationIntent, 0);
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Inregistrare persoane din apropiere")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .build();
-        startForeground(1, notification);
+        Log.d(TAG, "serviceExtendedClass: onStartCommand() called");
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "serviceExtendedClass: onDestroy() called");
         Nearby.getMessagesClient(this).unpublish(message);
         Nearby.getMessagesClient(this).unsubscribe(messageListener);
+        stopSelf();
     }
 
     private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel serviceChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Foreground Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(serviceChannel);
+        Log.d(TAG, "serviceExtendedClass: createNotificationChannel() called");
+
+        Intent serviceIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, serviceIntent, 0);
+
+
+        notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Social Distancing Scanner")
+                .setContentText("Text herejrklajflk ")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+    }
+
+    public void playTone() {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+        mp.start();
+    }
+
+
+    public void updateLastLoc() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    public void requestTracking() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.d(TAG, "onLocationChanged: Location has changed");
+                        message = new Message("Hi there".getBytes());
+                        Nearby.getMessagesClient(getBaseContext()).publish(message);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, (float) 0.5, locationListener);
+            }
+        };
+
+        r.run();
+    }
+
+    //Returns distance in meters between current loc and provide lat/lon combo
+    public double getGapFromMessage (Message message) {
+        String[] coords = message.toString().split("\\s+");
+        Location otherLoc = new Location(LocationManager.GPS_PROVIDER);
+        updateLastLoc();    //Get most recent loc
+
+        //Lat + Lon
+        otherLoc.setLatitude(Double.parseDouble(coords[0]));
+        otherLoc.setLongitude(Double.parseDouble(coords[1]));
+
+        return lastLoc.distanceTo(otherLoc);
     }
 
 }
