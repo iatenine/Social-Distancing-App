ef05e4b230469a3b2aaf6fdcce963e6a24d175b3
Jack Linhart <iatenine@gmail.com>
2020-04-05 17:58:01 -0600

    kinda working now

--- app/src/main/java/com/example/locationtestproject/MainActivity.java
+++ app/src/main/java/com/example/locationtestproject/MainActivity.java
@@ -1,36 +1,80 @@
 package com.example.locationtestproject;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Switch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Main Logger";
+    private static final String STANDARD = "STD-COVID-SD";
+
+    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+    MediaPlayer mp;
+
+    private MessageListener messageListener;
+    private Message message;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location lastLoc;
+
+    final int THRESHOLD = 3;
     final int FREQUENT_UDPATES = 1000;
     final int BATTERY_SAVER = 5000;
     Intent i;
     Boolean boolCautious = true;
 
-    Activity masterClass = this;
-
+    //Lifecycle methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(getApplicationContext(), notification);
         i = new Intent(getApplicationContext(), serviceExtendedClass.class);
     }
 
-    /*
-    * UI Functions
-    *
-    * */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setUpNearby();
+        setUpGPS();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Nearby.getMessagesClient(this).unpublish(message);
+        Nearby.getMessagesClient(this).unsubscribe(messageListener);
+    }
+
+    private String constructLocString(Location l){
+        String ret = STANDARD + " " + String.valueOf(l.getLatitude()) + " " + String.valueOf(l.getLongitude());
+        return ret;
+    }
+
+    //UI Functions
     public void startBroadcast(View view){
         ContextCompat.startForegroundService(getApplicationContext(), i);
     }
@@ -49,4 +93,123 @@ public class MainActivity extends AppCompatActivity {
         Switch cautiousSwtich = findViewById(R.id.switchConserve);
         boolCautious = cautiousSwtich.isChecked();
     }
+
+    //Math/parsing functions
+    private boolean isValidMsg(String[] s){
+        Log.d(TAG, "isValidMsg: length: " + String.valueOf(s.length));
+        Log.d(TAG, "isValidMsg: s[0]: " + (s[0]));
+        Log.d(TAG, "isValidMsg: standard: " + STANDARD);
+
+        if(s.length == 3 && s[0].compareTo(STANDARD) == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public double getDistToMsg (Message message) {
+        String[] coords = message.toString().split("\\s+");
+        if (isValidMsg(coords) == false)
+            return 999999;
+        Location otherLoc = new Location(LocationManager.GPS_PROVIDER);
+        //Lat + Lon
+        otherLoc.setLatitude(Double.parseDouble(coords[1]));
+        otherLoc.setLongitude(Double.parseDouble(coords[2]));
+
+        return lastLoc.distanceTo(otherLoc);
+    }
+
+    //Override functions
+    public void setUpNearby() {
+        Log.d(TAG, "setUpNearby: Called");
+        message = new Message("Init".getBytes());
+        messageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                String msg_str = new String(message.getContent());
+                Log.d(TAG, "Found message: " + new String(message.getContent()));
+                if(isValidMsg(msg_str.split("\\s+")) == true){
+                    if(getDistToMsg(message) > THRESHOLD){
+                        playTone();
+                        Log.d(TAG, "onFound: Too close!");
+                    }
+                    else {
+                        Log.d(TAG, "onFound: Far enough");
+                        stopTone();
+                    }
+                }
+                else
+                    Log.d(TAG, "onFound: Invalid message");
+            }
+
+            @Override
+            public void onLost(Message message) {
+                Log.d(TAG, "Lost sight of message: " + new String(message.getContent()));
+                stopTone();
+            }
+
+        };
+
+        Nearby.getMessagesClient(this).subscribe(messageListener);
+        Nearby.getMessagesClient(this).publish(message);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Not sure what to do with this...
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    public void setUpGPS() {
+        Log.d(TAG, "setUpGPS: Called");
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            return;
+        }
+        lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(lastLoc != null)
+            message = new Message(constructLocString(lastLoc).getBytes());
+        Log.d(TAG, "Initial Location: " + new String(message.getContent()));
+        Nearby.getMessagesClient(this).publish(message);
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d(TAG, "onLocationChanged: Publishing location");
+                lastLoc = location;
+                message = new Message(constructLocString(location).getBytes());
+                Nearby.getMessagesClient(getApplicationContext()).publish(message);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, (float) 0.2, locationListener);
+    }
+
+    public void playTone() {
+        mp.setLooping(true);
+        mp.start();
+    }
+
+    public void stopTone(){
+        mp.stop();
+    }
+
 }
--- app/src/main/java/com/example/locationtestproject/serviceExtendedClass.java
+++ app/src/main/java/com/example/locationtestproject/serviceExtendedClass.java
@@ -1,50 +1,21 @@
 package com.example.locationtestproject;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.IntentService;
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.media.MediaPlayer;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.WorkerThread;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.messages.Message;
-import com.google.android.gms.nearby.messages.MessageListener;
 
 public class serviceExtendedClass extends Service {
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
     public static final int NOT_ID = 6450;
     private static final String TAG = "Service Logger";
 
-    private MessageListener messageListener;
-    private Message message;
-
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private Location lastLoc;
-
     private Notification notification;
-    private NotificationChannel notificationChannel;
 
 
     @Nullable
@@ -56,26 +27,11 @@ public class serviceExtendedClass extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-
         Log.d(TAG, "serviceExtendedClass: onCreate() called");
+
         createNotificationChannel();
         startForeground(NOT_ID, notification);
 
-        messageListener = new MessageListener() {
-            @Override
-            public void onFound(Message message) {
-                Log.d(TAG, "onFound: Found message: " + message.toString());
-            }
-
-            @Override
-            public void onLost(Message message) {
-            }
-        };
-
-
-        Nearby.getMessagesClient(getBaseContext()).subscribe(messageListener);
-        requestTracking();
-        updateLastLoc();
     }
 
     @Override
@@ -88,8 +44,6 @@ public class serviceExtendedClass extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "serviceExtendedClass: onDestroy() called");
-        Nearby.getMessagesClient(this).unpublish(message);
-        Nearby.getMessagesClient(this).unsubscribe(messageListener);
         stopSelf();
     }
 
@@ -100,7 +54,7 @@ public class serviceExtendedClass extends Service {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, serviceIntent, 0);
 
 
-        notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+        notification = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
                 .setContentTitle("Social Distancing Scanner")
                 .setContentText("Text herejrklajflk ")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
@@ -108,73 +62,15 @@ public class serviceExtendedClass extends Service {
                 .build();
     }
 
+    /*
     public void playTone() {
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
         mp.start();
     }
 
-
-    public void updateLastLoc() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    }
-
-    public void requestTracking() {
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        Log.d(TAG, "onLocationChanged: Location has changed");
-                        message = new Message("Hi there".getBytes());
-                        Nearby.getMessagesClient(getBaseContext()).publish(message);
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-
-                    }
-                };
-
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, (float) 0.5, locationListener);
-            }
-        };
-
-        r.run();
-    }
-
+     */
+    /*
     //Returns distance in meters between current loc and provide lat/lon combo
     public double getGapFromMessage (Message message) {
         String[] coords = message.toString().split("\\s+");
@@ -187,5 +83,7 @@ public class serviceExtendedClass extends Service {
 
         return lastLoc.distanceTo(otherLoc);
     }
+
+     */
 
 }
